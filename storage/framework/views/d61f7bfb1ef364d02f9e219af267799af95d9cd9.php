8d09d4250e6ee5ea8962823d4e7f2dab3e1591f3
amir huzaifah <69615018+Huzaifah987@users.noreply.github.com>
2021-07-24 11:42:59 +0800

    laravel administration

---
+++ storage/framework/views/d61f7bfb1ef364d02f9e219af267799af95d9cd9.php
@@ -0,0 +1,28 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+        
+        <div class="copyright text-center text-xl-left text-muted"> &copy; <?php echo e(now()->year); ?> Copyright. Allright Preserve:
+            <a href=""> Amir Huzaifah</a>
+        </div>
+    </div>
+    <div class="col-xl-6">
+        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+            <li class="nav-item">
+                <a href="" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">MIT License</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<?php /**PATH /home/vagrant/administration-system/resources/views/layouts/footers/nav.blade.php ENDPATH**/ ?>
